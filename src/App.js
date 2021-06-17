972d81d23f7062165b320e11287b4852cf7c6b8f
Samuel de Carvalho Pereira <samuel.pereira@fitbank.com.br>
2021-06-17 14:24:57 -0300

    Criação de exemplos de utilização de hooks simples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived)
+  }, []);
+
+  function handlePositionReceived({ coords }){
+    const { latitude, longitude } = coords;
+    setLocation({latitude, longitude});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   Latitude: {location.latitude} <br/>
+   Longitude:  {location.longitude}
+   </>
   );
 }
 
 export default App;
+
+
+/**
+ * const [repositories, setRepositories] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch(
+      "https://api.github.com/users/samuelcarvalhodev/repos"
+    );
+    const data = await response.json();
+    setRepositories(data);
+  }, []);
+
+  useEffect( () => {
+   const filtered = repositories.filter(repo => repo.favorite);
+   document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+    console.log(repositories);
+  }
+
+
+
+
+
+
+
+
+
+  return:
+   <div>
+      <ul>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>Favorito</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+ */
